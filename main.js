f28b5835b2b2d2491e29ec64ea6a135c1c1d6c14
Roger Cassahi <contatorooger@gmail.com>
2021-08-30 11:33:24 -0300

    organização do código

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ let textarea = document.querySelector("textarea");
 let tarefas = document.querySelector("#tarefas");
 let tarefasConcluidas = document.querySelector("#tarefas-concluidas");
 const ul = document.querySelector("ul");
-let concluido = 0;
-let tarefaCriada = 0;
 
 function createItem() {
   if (textarea.value == "") {
@@ -11,36 +9,33 @@ function createItem() {
     alert("Por favor, insira uma tarefa");
     return;
   } else {
-    console.log(textarea);
     const tarefa = document.createElement("li");
     const icon = document.createElement("i");
     ul.appendChild(tarefa);
     tarefa.innerHTML = textarea.value;
     tarefa.appendChild(icon);
-    icon.addEventListener('click', removeItem);
+    icon.addEventListener("click", removeItem);
     tarefa.addEventListener("click", markItem);
     tarefa.setAttribute("class", "tarefa");
     icon.setAttribute("class", "bi bi-x-square");
-    textarea.value = ''
-    tarefas.innerHTML = `Tarefas: ${ul.childNodes.length += 1}`
+    textarea.value = "";
+    tarefas.innerHTML = `Tarefas: ${(ul.childNodes.length += 1)}`;
   }
 }
 
 function removeItem(event) {
   event.target.parentNode.classList.add("delete");
- setTimeout(() =>{
-   event.target.parentNode.remove()
- },600)}
+  setTimeout(() => {
+    event.target.parentNode.remove();
+  }, 600);
+}
 
 function markItem(event) {
   event.target.classList.toggle("mark");
- }
- 
- document.addEventListener('keydown', function (event) {
-  if (event.keyCode == 13){
-    createItem()
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 13) {
+    createItem();
   }
- });
-
-
-
+});
